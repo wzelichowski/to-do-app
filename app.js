33d86b3386b313dde8ee102c9d34396c1b727efe
wzelichowski <wzelichowski@gmail.com>
2018-10-03 22:01:55 +0200

    Logic for search and hide filters

--- app.js
+++ app.js
@@ -15,35 +15,62 @@ const todos = [{
   completed: false
 }]
 
-const incompleteTodos = todos.filter(function(todo) {
-  return !todo.completed
+const filters = {
+  searchText: '',
+  hideCompleted: false
+}
+
+const renderTodos = function(todos, filters) {
+  let filteredTodos = todos.filter(function(todo) {
+    return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+  })
+
+  filteredTodos = filteredTodos.filter(function(todo) {
+    if(filters.hideCompleted) {
+      return !todo.completed
+    } else {
+      return true
+    }
+  })
+
+  const incompleteTodos = filteredTodos.filter(function(todo) {
+    return !todo.completed
+  })
+  
+  document.querySelector('#todos').innerHTML = ''
+
+  const summary = document.createElement('h2')
+  summary.textContent = `You have ${incompleteTodos.length} todos left`
+  document.querySelector('#todos').appendChild(summary)
+  
+  filteredTodos.forEach(function(todo) {
+    const p = document.createElement('p')
+    p.textContent = todo.text
+    document.querySelector('#todos').appendChild(p)
+  })
+}
+
+renderTodos(todos, filters)
+
+document.querySelector('#search-text').addEventListener('input', function(e) {
+  filters.searchText = e.target.value
+  renderTodos(todos, filters)
 })
 
-const summary = document.createElement('h2')
-summary.textContent = `You have ${incompleteTodos.length} todos left`
-document.querySelector('body').appendChild(summary)
-
-todos.forEach(function(todo) {
-  const p = document.createElement('p')
-  p.textContent = todo.text
-  document.querySelector('body').appendChild(p)
+document.querySelector('#new-todo-form').addEventListener('submit', function(e) {
+  e.preventDefault()
+  todos.push({
+    text: e.target.elements.addTodo.value,
+    completed: false
+  })
+  renderTodos(todos, filters)
+  e.target.elements.addTodo.value = ''
 })
 
-document.querySelector('#add-note').addEventListener('click', function(e) {
-  console.log('It works!')
+document.querySelector('#hide-completed').addEventListener('change', function(e) {
+  filters.hideCompleted = e.target.checked
+  renderTodos(todos, filters)
 })
-
-document.querySelector('#add-text').addEventListener('input', function(e) {
-  console.log(e.target.value)
-})
-
-
-
-
-
-
-
-
 
 
 
